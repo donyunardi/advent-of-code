162cfdf09668d3850f40c45138cbdd06a5bf549b
Dony Unardi <donyunardi@gmail.com>
2022-12-05 17:35:49 -0800

    fix path

--- 2022/p1.py
+++ 2022/p1.py
@@ -1,6 +1,8 @@
+import os
+from os.path import join
 total_calories = []
 temp = 0
-with open("/Users/unardid/Documents/experiment/gitproject/advent-of-code/2022/p1_input.txt", "r") as input_file:
+with open(join(os.getcwd(), "2022/p1_input.txt"), "r") as input_file:
     input_data = input_file.read().splitlines()
 
 for calories in input_data:
